db3fc9f8c5255672a7b289f96ca26f006bfaa160
praveen <kmpraveen25@gmail.com>
2022-01-02 19:42:02 +0000

    updated integration cases

--- src/test/java/co/test/TestBase.java
+++ src/test/java/co/test/TestBase.java
@@ -3,6 +3,7 @@ package co.test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
@@ -16,7 +17,9 @@ public class TestBase {
     public void setUp(){
         if(driver==null){
             WebDriverManager.chromedriver().setup();
-            driver=new ChromeDriver();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless");
+            driver = new ChromeDriver(chromeOptions);
             driver.get("http://www.gmail.com");
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
